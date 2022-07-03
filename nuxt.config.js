d3e2f0985714f53f619bd224a8e743546c73bd40
Noel Schmidt <noel_schmidt@outlook.de>
2022-07-03 07:46:38 +0200

    Added nuxt.js server-side boilerplate

---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+export default {
+  target: 'server',
+  head: {
+    title: 'nuxt-server-boilerplate',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+  ],
+
+  plugins: [
+  ],
+
+  components: true,
+
+  buildModules: [
+    '@nuxt/typescript-build'
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxt/image',
+  ],
+
+  axios: {
+    baseURL: '/'
+  },
+
+  build: {
+    optimizeCSS: true,
+    html: {
+      minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      removeEmptyElements: true,
+      minify: true,
+    },
+    cssSourceMap: true,
+    optimization: {
+      removeEmptyChunks: true
+    },
+    splitChunks: true
+  }
+}
